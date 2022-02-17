da57ce67b61106207e6a35a079f02b12d87b5d29
Anna Tuban <mshannahstarling@gmail.com>
2022-02-18 00:05:29 +0300

    feat: add sign-out

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import headerLogo from '../images/logo.svg';
 
-function Header({ ...props }) {
+function Header({ onSignOut, email, loggedIn, ...props }) {
   const location = useLocation();
   const REGISTER_ROUTE = '/sign-up';
   const LOGIN_ROUTE = '/sign-in';
   const isRegister = location.pathname === LOGIN_ROUTE;
-  const isLogin = location.pathname !== LOGIN_ROUTE && location.pathname !== REGISTER_ROUTE;
-  const loginText = isLogin ? 'Выйти' : isRegister ? 'Регистрация' : 'Войти';
+  const loginText = loggedIn ? 'Выйти' : isRegister ? 'Регистрация' : 'Войти';
 
   return (
     <header className='header page__header'>
@@ -20,8 +19,8 @@ function Header({ ...props }) {
         />
       </Link>
       <div className='header__info'>
-        {isLogin && <p className='header__profile-email'>email@mail.com</p>}
-        <Link to={isRegister ? REGISTER_ROUTE : LOGIN_ROUTE} className='header__link link-opacity'>
+        {loggedIn && <p className='header__profile-email'>{email}</p>}
+        <Link onClick={onSignOut} to={isRegister ? REGISTER_ROUTE : LOGIN_ROUTE} className='header__link link-opacity'>
           {loginText}
         </Link>
       </div>
