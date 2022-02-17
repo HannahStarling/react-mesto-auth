0d63dd74cab137c2818491fe54817a834fc2671d
Anna Tuban <mshannahstarling@gmail.com>
2022-02-17 16:00:18 +0300

    fix: button in Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,10 +6,12 @@ function Form({ formBlockClass, onSubmit, name, hasInput, title, children, butto
       <h2 className={`${formBlockClass}__title ${hasInput ? `` : `${formBlockClass}__title_type_noinputs`}`.trim()}>
         {title}
       </h2>
-      <fieldset className={`${formBlockClass}__input-container`}>{children}</fieldset>
-      <button className={`button ${formBlockClass}__btn ${formBlockClass}__btn_action_submit`} type='submit'>
-        {buttonText}
-      </button>
+      <fieldset className={`${formBlockClass}__input-container`}>
+        {children}
+        <button className={`button ${formBlockClass}__btn ${formBlockClass}__btn_action_submit`} type='submit'>
+          {buttonText}
+        </button>
+      </fieldset>
     </form>
   );
 }
